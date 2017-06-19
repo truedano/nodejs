b0df70726f637e5ca8524b94992be85003874c15
truedano <truedano@gmail.com>
2017-06-19 15:48:35 +0800

    add order

---
+++ order/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var port = 3000;
+
+//route//////////////////////////////////////////////////////////////////
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/view/index.html'));
+});
+
+app.get('/check_user', function (req, res) {
+    res.send(JSON.stringify({success: true}));
+});
+
+app.get('/check_seller', function (req, res) {
+    res.send(JSON.stringify({success: true}));
+});
+
+app.get('/check_admin', function (req, res) {
+    res.send(JSON.stringify({success: true}));
+});
+
+app.get('*', function(req, res){
+    res.send('Page not found!', 404);
+});
+//route//////////////////////////////////////////////////////////////////
+
+//static/////////////////////////////////////////////////////////////////
+app.use(express.static('public'));
+
+app.listen(port, function () {
+    console.log('Server listening on port '+port+" "+new Date());
+});
