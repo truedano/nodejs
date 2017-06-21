f7f33e8268cd86d4caa8bccbc9e53c2f71303a96
root <truedano@gmail.com>
2017-06-21 14:27:34 +0800

    add write mongodb table menu

--- order/app.js
+++ order/app.js
@@ -4,37 +4,121 @@ var path = require("path");
 var port = 3000;
 var mydbClass = require("./model/mydb.js");
 var mydb = new mydbClass();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //route//////////////////////////////////////////////////////////////////
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/view/index.html'));
 });
 
+app.get('/user',function(req, res){
+    res.sendFile(path.join(__dirname+'/view/user.html'));
+});
+
+app.post('/user',function(req, res){
+    console.log("post to user");
+});
+
 app.get('/check_user', function (req, res) {
     res.send(JSON.stringify({success: true}));
+});
+
+app.get('/seller',function(req, res){
+    res.sendFile(path.join(__dirname+'/view/seller.html'));
+});
+
+app.post('/seller',function(req, res){
+    console.log("post to seller");
 });
 
 app.get('/check_seller', function (req, res) {
     res.send(JSON.stringify({success: true}));
 });
 
+app.get('/admin',function(req, res){
+    res.sendFile(path.join(__dirname+'/view/admin.html'));
+});
+
+app.post('/admin',function(req, res){
+    console.log("post to admin");
+    var obj={
+        number:req.body.number,
+        name:req.body.name,
+        price:req.body.price,
+        descript:req.body.descript,
+        time: req.body.time
+    };
+    mydb.insert("menu",obj,function(err,obj){
+        res.send(JSON.stringify({success: true}));
+    });
+});
+
 app.get('/check_admin', function (req, res) {
     res.send(JSON.stringify({success: true}));
 });
 
-app.get('*', function(req, res){
-    res.send('Page not found!', 404);
+app.get("/mydb",function(req, res){
+    console.log("get mydb");
+    var dbname = req.query.dbname;
+    var type = req.query.type;
+    if( type == "all" ){
+        mydb.findAll(dbname,function(result){
+            res.send(JSON.stringify(result));
+        });
+    }else if( type == "one" ){
+
+    }
+});
+
+app.post("/mydb",function(req, res){
+    var dbname = req.query.dbname;
+    var type = req.query.type;
+    console.log("post mydb : type :"+type);
+    if( type == "delall" ){
+        mydb.drop(dbname,function(err, delOK){
+            res.send(JSON.stringify({success: true}));
+        });
+    }else if( type == "delone" ){
+        console.log("delone");
+        var myquery = {
+            number:req.body.number,
+            name:req.body.name,
+            price:req.body.price,
+            descript:req.body.descript,
+            time: req.body.time
+        };
+        mydb.remove(dbname,myquery,function(err, obj){
+            res.send(JSON.stringify({success: true}));
+        });
+    }else if( type == "modifyone" ){
+        console.log("modifyone");
+        var myquery = {
+            time: req.body.time
+        };
+        var newvalues = {
+            number:req.body.number,
+            name:req.body.name,
+            price:req.body.price,
+            descript:req.body.descript,
+            time: req.body.time
+        };
+        mydb.update(dbname,myquery,newvalues,function(err, obj){
+            res.send(JSON.stringify({success: true}));
+        });
+    }
 });
 //route//////////////////////////////////////////////////////////////////
 
 //static/////////////////////////////////////////////////////////////////
-app.use(express.static('public'));
+app.use('/static', express.static(__dirname + '/static'));
 
 app.listen(port, function () {
     console.log('Server listening on port '+port+" "+new Date());
 
-    mydb.findAll("test",function(result){
-        console.log(result);
-    });
-
+    //mydb.findAll("menu",function(result){
+    //    console.log(result);
+    //});
 });
--- order/model/mydb.js
+++ order/model/mydb.js
@@ -10,6 +10,41 @@ module.exports = function(){
                 db.close();
             });
         });
-    }
+    };
 
+    this.insert = function(tableName,obj,callback){
+        MongoClient.connect(url,function(err,db){
+            db.collection(tableName).insertOne(obj,function(err,obj){
+                callback(err,obj);
+                db.close();
+            });
+        });
+    };
+
+    this.drop = function(tableName,callback){
+        MongoClient.connect(url,function(err,db){
+            db.collection(tableName).drop(function(err, delOK){
+                callback(err, delOK);
+                db.close();
+            });
+        });
+    };
+
+    this.remove = function(tableName,myquery,callback){
+        MongoClient.connect(url,function(err,db){
+            db.collection(tableName).remove(myquery,function(err, obj){
+                callback(err, obj);
+                db.close();
+            });
+        });
+    };
+
+    this.update = function(tableName,myquery,newvalues,callback){
+        MongoClient.connect(url,function(err,db){
+            db.collection(tableName).update(myquery,newvalues,function(err, obj){
+                callback(err, obj);
+                db.close();
+            });
+        });
+    };
 }
