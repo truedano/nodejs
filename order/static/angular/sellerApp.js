d81b408cf2c13a45407c151b2cdda46f093f2f6d
truedano <truedano@gmail.com>
2017-06-23 10:15:11 +0800

    modify sort part

--- order/static/angular/sellerApp.js
+++ order/static/angular/sellerApp.js
@@ -1,6 +1,7 @@
 var app = angular.module('sellerApp', []);
 
 app.controller('sellerCtrl', function($scope, $http) {
+    var sortType = -1;
 
     $scope.completeOrder = function(x){
         var dataobj = {
@@ -9,7 +10,7 @@ app.controller('sellerCtrl', function($scope, $http) {
             data: x
         };
         $http(dataobj).then(function(response){
-            getDb($http,"userorder","allsort","time","-1",function(result){
+            getDb($http,"userorder","allsort","time",sortType,function(result){
                 $scope.userorderresult = result;
             });
             console.log(dataobj.method,dataobj.url,"success");
@@ -25,7 +26,7 @@ app.controller('sellerCtrl', function($scope, $http) {
             data: x
         };
         $http(dataobj).then(function(response){
-            getDb($http,"userorder","allsort","time","-1",function(result){
+            getDb($http,"userorder","allsort","time",sortType,function(result){
                 $scope.userorderresult = result;
             });
             console.log(dataobj.method,dataobj.url,"success");
@@ -34,7 +35,14 @@ app.controller('sellerCtrl', function($scope, $http) {
         });
     };
 
-    getDb($http,"userorder","allsort","time","-1",function(result){
+    $scope.clickSort = function(sortTarget){
+        sortType == -1?sortType = 1:sortType = -1;
+        getDb($http,"userorder","allsort",sortTarget,sortType,function(result){
+            $scope.userorderresult = result;
+        });
+    };
+
+    getDb($http,"userorder","allsort","time",sortType,function(result){
         $scope.userorderresult = result;
     });
 
